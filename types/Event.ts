97ffffa94fb5ad4093c9ed395063bee4006433f6
Alexiy Rybin <0xtapnisu@gmail.com>
2023-01-05 13:15:24 +0700

    Fixed warn for cli

--- types/Event.ts
+++ types/Event.ts
@@ -1,11 +1,11 @@
 import ExtendedClient from "@core";
-import { ClientEvents } from "harmony";
+import { ClientEvents, Message } from "harmony";
 
 type ArgsKeys = keyof ClientEvents;
 type Args = ClientEvents[ArgsKeys];
 
 interface Run {
-  (client: ExtendedClient, ...args: Args): any;
+  (client: ExtendedClient, ...args: Args): Promise<Message | void> | Message | void;
 }
 
 export interface Event {
