673bccf097102c7e6e90fa5e932859b270c1ca98
Alexiy Rybin <0xtapnisu@gmail.com>
2023-01-05 13:05:59 +0700

    added redirect to github

--- utils/server.ts
+++ utils/server.ts
@@ -1,18 +1,5 @@
-const server = async (request: Request): Promise<Response> => {
-  let { pathname } = new URL(request.url);
-
-  if (pathname.endsWith("/")) pathname += "index.html";
-
-  try {
-    const file = await Deno.open("./static" + decodeURIComponent(pathname), {
-      read: true,
-    });
-    const readableStream = file.readable;
-
-    return new Response(readableStream, { status: 200 });
-  } catch {
-    return new Response("404 Not Found", { status: 404 });
-  }
+const server = (): Response => {
+  return Response.redirect("https://github.com/tapris-bot/tapris", 302);
 };
 
 export default server;
