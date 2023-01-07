74cf42c53f3fc3c5d993f3c9183825f6380ed7a6
Alexiy Rybin <0xtapnisu@gmail.com>
2023-01-07 11:57:09 +0700

    fix image

--- commands/utils/color.ts
+++ commands/utils/color.ts
@@ -35,23 +35,23 @@ const command: Command = {
 
     ctx.fillStyle = colorString;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.font = "50px JetBrains Mono";
+    ctx.font = "50px monospace";
     ctx.fillStyle = "#ffffff";
     ctx.textAlign = "center";
     ctx.fillText(colorString, 250, 200);
-    ctx.font = "50px JetBrains Mono";
+    ctx.font = "50px monospace";
     ctx.fillStyle = "#000000";
     ctx.textAlign = "center";
     ctx.fillText(colorString, 250, 350);
 
     const attachment = new MessageAttachment(
-      "ColorHexSend.jpg",
-      canvas.toBuffer("image/jpeg")
+      "ColorHexSend.png",
+      canvas.toBuffer("image/png")
     );
 
     const embed = new Embed()
       .setTitle(colorString)
-      .setImage("attachment://ColorHexSend.jpg");
+      .setImage("attachment://ColorHexSend.png");
 
     try {
       embed.setColor(colorString);
@@ -59,7 +59,9 @@ const command: Command = {
       embed.setColor(client.env.BOT_COLOR);
     }
 
-    return await interaction.reply({ embeds: [embed], files: [attachment] });
+    embed.attach(attachment);
+
+    return await interaction.reply({ embeds: [embed] });
   },
 };
 
