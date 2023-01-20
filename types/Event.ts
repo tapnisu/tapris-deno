0d5eaa954a13e0869358ea102edbdf3feb00ca60
Alexiy Rybin <0xtapnisu@gmail.com>
2023-01-20 22:29:38 +0700

    add todo

--- types/Event.ts
+++ types/Event.ts
@@ -7,10 +7,11 @@ type Args = ClientEvents[EventName];
 interface Run {
   (
     client: ExtendedClient,
+    // TODO: remove any here by fixing Args type
     // deno-lint-ignore no-explicit-any
     ...args: any[]
-    //...args: Args
-  ): Promise<Message | undefined | void> | Message | undefined | void;
+  ): //...args: Args
+  Promise<Message | undefined | void> | Message | undefined | void;
 }
 
 export interface Event {
