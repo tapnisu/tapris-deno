b1617e43d67947e54dddd930ccbb389edd896b56
Alexiy Rybin <0xtapnisu@gmail.com>
2023-02-02 15:56:55 +0700

    fix guildsOnly

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -1,24 +1,30 @@
 import ExtendedClient from "@core";
-import { Command, Component, Event } from "@types";
+import { Event } from "@types";
 import { Interaction } from "harmony";
 
 const event: Event = {
   name: "interactionCreate",
-  run: (client: ExtendedClient, interaction: Interaction) => {
+  run: async (client: ExtendedClient, interaction: Interaction) => {
     if (interaction.isApplicationCommand()) {
       const command = client.commands.get(interaction.name);
-      if (command) (command as Command).run(client, interaction);
 
-      return;
+      if (command) {
+        if (command.guildsOnly && !interaction.guild)
+          return await interaction.reply({
+            content: "Sorry, this command is only for guilds.",
+            ephemeral: true,
+          });
+
+        return command.run(client, interaction);
+      }
     }
 
     if (interaction.isMessageComponent()) {
       const component = client.components.find((component) =>
         component.customId.test(interaction.data.custom_id)
       );
-      if (component) (component as Component).run(client, interaction);
 
-      return;
+      if (component) return component.run(client, interaction);
     }
   },
 };
