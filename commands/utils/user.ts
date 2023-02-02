f018379fae2cfc228912ef6abdeb0eb2e1631696
Alexiy Rybin <0xtapnisu@gmail.com>
2023-02-02 16:15:40 +0700

    translate user

--- commands/utils/user.ts
+++ commands/utils/user.ts
@@ -1,6 +1,17 @@
 import { Command } from "@types";
 import { Embed, User } from "harmony";
 
+const commandLocales = {
+  en: {
+    unknownError: () => "Unknown error happened! :(",
+    bot: () => "`bot`",
+  },
+  ru: {
+    unknownError: () => "Произошла неизвестная ошибка! :(",
+    bot: () => "`бот`",
+  },
+};
+
 const command: Command = {
   name: "user",
   description: "Sends user information",
@@ -14,21 +25,26 @@ const command: Command = {
   ],
   run: async (client, interaction) => {
     const user: User | undefined = await client.users.get(
-      interaction.options.find((option) => option.name == "user")?.value,
+      interaction.options.find((option) => option.name == "user")?.value
     );
+
+    const locales = (await client.db.selectLocale(
+      interaction.guild?.id,
+      commandLocales
+    )) as typeof commandLocales.en;
 
     if (!user) {
       return interaction.reply({
-        content: "Unknown error happened! :(",
+        content: locales.unknownError(),
         ephemeral: true,
       });
     }
 
     const embed = new Embed()
       .setColor(client.env.BOT_COLOR)
-      .setTitle(`${user.tag} ${user.bot ? "[bot]" : ""}`)
+      .setTitle(`${user.tag} ${user.bot ? locales.bot() : ""}`)
       .setThumbnail(user.avatarURL())
-      .addFields({ name: "Id", value: user.id, inline: true });
+      .addFields({ name: "ID", value: user.id, inline: true });
 
     return interaction.reply({ embeds: [embed] });
   },
