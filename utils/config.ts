c59abb9a66ec9c489f61809e3ef2bd1fa62904df
Alexiy Rybin <0xtapnisu@gmail.com>
2023-02-02 16:02:49 +0700

    add types help to config.ts

--- utils/config.ts
+++ utils/config.ts
@@ -1,12 +1,33 @@
 import { config } from "dotenv";
 
-const inits = () => {
+interface Env {
+  BOT_TOKEN: string
+  BOT_COLOR: string
+
+  DATABASE_NAME: string
+
+  HOST: string
+  USERNAME: string
+  PASSWORD: string
+  DATABASE: string
+  PORT: string
+
+  FILE_PATH: string
+
+  SERVER_PORT: string
+
+  MODE: "DEPLOY" | "DENODEPLOY" | string
+
+  [index: string]: string
+}
+
+const inits = (): Env => {
   if (
     Deno.env.get("MODE") === "DEPLOY" || Deno.env.get("MODE") === "DENODEPLOY"
   ) {
-    return Deno.env.toObject();
+    return Deno.env.toObject() as Env;
   } else {
-    return config();
+    return config() as Env;
   }
 };
 
