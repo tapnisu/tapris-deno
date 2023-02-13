52c2c372e93a232bdc23c950155b960db3fcfa62
Alexiy Rybin <0xtapnisu@gmail.com>
2023-02-13 18:50:26 +0700

    add test for symbols test

--- utils/generatePassword_test.ts
+++ utils/generatePassword_test.ts
@@ -1,8 +1,24 @@
-import generatePassword from "@utils/generatePassword.ts";
+import generatePassword, { defaultCharset } from "@utils/generatePassword.ts";
 import { assertEquals } from "std/testing/asserts.ts";
 
 Deno.test("generatePassword length test", () => {
   assertEquals(generatePassword(8).length, 8);
   assertEquals(generatePassword(16).length, 16);
   assertEquals(generatePassword(4).length, 4);
+});
+
+Deno.test("generatePassword symbols test", () => {
+  generatePassword(8)
+    .split("")
+    .forEach((char) => {
+      assertEquals(defaultCharset.includes(char), true);
+    });
+
+  const customCharset = "abcdefg1234";
+
+  generatePassword(16, customCharset)
+    .split("")
+    .forEach((char) => {
+      assertEquals(customCharset.includes(char), true);
+    });
 });
