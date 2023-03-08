2d9fc9f172d6555beb206937366a37fcfc55d2d5
Alexiy Rybin <0xtapnisu@gmail.com>
2023-03-08 08:17:25 +0700

    Make events small

--- events/mod.ts
+++ events/mod.ts
@@ -4,17 +4,18 @@ import guildDelete from "@events/guildDelete.ts";
 import interactionCreate from "@events/interactionCreate.ts";
 import ready from "@events/ready.ts";
 import reconnect from "@events/reconnect.ts";
-import { Event } from "@typings/mod.ts";
 
-const Register = (client: ExtendedClient, event: Event) => {
-  client.events.set(event.name, event);
-  client.on(event.name, event.run.bind(null, client));
-};
+const events = [
+  ready,
+  interactionCreate,
+  guildCreate,
+  guildDelete,
+  reconnect,
+];
 
 export default (client: ExtendedClient) => {
-  Register(client, ready);
-  Register(client, reconnect);
-  Register(client, interactionCreate);
-  Register(client, guildCreate);
-  Register(client, guildDelete);
+  events.forEach((event) => {
+    client.events.set(event.name, event);
+    client.on(event.name, event.run.bind(null, client));
+  });
 };
