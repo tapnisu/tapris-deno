5ad814788298b09b32ef6bda44baf1e2f6be91da
Alexiy Rybin <0xtapnisu@gmail.com>
2023-03-08 08:57:53 +0700

    fix types

--- typings/Event.ts
+++ typings/Event.ts
@@ -1,30 +1,4 @@
-import ExtendedClient from "@core";
-import {
-  ApplicationCommandInteraction,
-  ClientEvents,
-  Interaction,
-  Message,
-} from "harmony/mod.ts";
+import { EventBuilder as Event } from "@builders/mod.ts";
 
-type EventName = keyof ClientEvents;
-type Args = ClientEvents[EventName];
+export { Event };
 
-interface Run {
-  (
-    client: ExtendedClient,
-    // TODO: remove any here by fixing Args type
-    // deno-lint-ignore no-explicit-any
-    ...args: any[]
-  ): //...args: Args
-  | Promise<
-    ApplicationCommandInteraction | Interaction | Message | undefined | void
-  >
-  | Message
-  | undefined
-  | void;
-}
-
-export interface Event {
-  name: EventName;
-  run: Run;
-}
