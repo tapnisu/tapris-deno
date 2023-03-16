e9ff29b2a441e02b9a6c6ffcb7d0efa53a8fb164
Alexiy Rybin <0xtapnisu@gmail.com>
2023-03-16 18:03:51 +0700

    Rewrite genshinCodes

--- src/commands/api/genshinCodes.ts
+++ src/commands/api/genshinCodes.ts
@@ -1,7 +1,18 @@
-import { Code, Command } from "@typings/mod.ts";
+import { CommandBuilder } from "@builders/mod.ts";
+import { Code, LocaleRecords } from "@typings/mod.ts";
 import { ActionRowComponent, Embed } from "harmony/mod.ts";
 
-const commandLocales = {
+interface GenshinCodesLocales extends LocaleRecords {
+  activateButton: () => string;
+  embedTitle: () => string;
+  description: () => string;
+}
+
+const command = new CommandBuilder<GenshinCodesLocales>().setName(
+  "genshincodes",
+).setDescription(
+  "Get valid codes for Genshin Impact",
+).setLocales({
   en: {
     activateButton: () => "Activate",
     embedTitle: () => "Codes for Genshin Impact",
@@ -12,58 +23,47 @@ const commandLocales = {
     embedTitle: () => "Промокоды для Genshin Impact",
     description: () => "Вы можете активировать их в игре и получить награды!",
   },
-};
+}).setRun(async (client, interaction, locale) => {
+  const response = await (
+    await fetch(
+      "https://raw.githubusercontent.com/ataraxyaffliction/gipn-json/main/gipn.json",
+    )
+  ).json();
 
-const command: Command = {
-  name: "genshincodes",
-  description: "Get valid codes for Genshin Impact",
-  run: async (client, interaction) => {
-    const response = await (
-      await fetch(
-        "https://raw.githubusercontent.com/ataraxyaffliction/gipn-json/main/gipn.json",
-      )
-    ).json();
+  const codes = response.CODES;
+  const url = "https://genshin.hoyoverse.com/en/gift";
 
-    const locales = (await client.db.selectLocale(
-      commandLocales,
-      interaction.guild?.id,
-    )) as typeof commandLocales.en;
+  const embed = new Embed()
+    .setColor(client.botColor)
+    .setTitle(locale.embedTitle())
+    .setDescription(locale.description())
+    .setURL(url);
 
-    const codes = response.CODES;
-    const url = "https://genshin.hoyoverse.com/en/gift";
+  codes.forEach((code: Code) => {
+    if (code.is_expired == false) {
+      let rewards: string[] = [];
 
-    const embed = new Embed()
-      .setColor(client.botColor)
-      .setTitle(locales.embedTitle())
-      .setDescription(locales.description())
-      .setURL(url);
+      code.reward_array.forEach((reward) => {
+        rewards = [...rewards, `${reward.name}: ${reward.count}`];
+      });
 
-    codes.forEach((code: Code) => {
-      if (code.is_expired == false) {
-        let rewards: string[] = [];
+      embed.addField(code.code, rewards.join("\n"), true);
+    }
+  });
 
-        code.reward_array.forEach((reward) => {
-          rewards = [...rewards, `${reward.name}: ${reward.count}`];
-        });
+  const buttonsRow: ActionRowComponent = {
+    type: 1,
+    components: [
+      {
+        type: 2,
+        url: url,
+        label: locale.activateButton(),
+        style: 5,
+      },
+    ],
+  };
 
-        embed.addField(code.code, rewards.join("\n"), true);
-      }
-    });
-
-    const buttonsRow: ActionRowComponent = {
-      type: 1,
-      components: [
-        {
-          type: 2,
-          url: url,
-          label: locales.activateButton(),
-          style: 5,
-        },
-      ],
-    };
-
-    return interaction.reply({ embeds: [embed], components: [buttonsRow] });
-  },
-};
+  return interaction.reply({ embeds: [embed], components: [buttonsRow] });
+});
 
 export default command;
