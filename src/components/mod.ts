5371c80b0d630172988b77cdbe55a0a755c53dff
Alexiy Rybin <0xtapnisu@gmail.com>
2023-03-17 15:03:56 +0700

    Small components registering

--- src/components/mod.ts
+++ src/components/mod.ts
@@ -1,13 +1,14 @@
 import deleteMessage from "@components/delete_message.ts";
 import passwordNew from "@components/password-new.ts";
 import Client from "@core";
-import { Component } from "@typings/mod.ts";
 
-const Register = (client: Client, component: Component) => {
-  client.components.set(component.customId, component);
-};
+const components = [
+  deleteMessage,
+  passwordNew,
+];
 
 export default (client: Client) => {
-  Register(client, passwordNew);
-  Register(client, deleteMessage);
+  components.forEach((component) =>
+    client.components.set(component.customId, component)
+  );
 };
