e5199c02217e52e72def957b14433df5b856b661
Alexiy Rybin <0xtapnisu@gmail.com>
2023-05-21 12:04:18 +0700

    Use new system

--- src/commands/utils/setLanguage.ts
+++ src/commands/utils/setLanguage.ts
@@ -1,12 +1,11 @@
 import { LocaleNames, TaprisCommand } from "@framework/mod.ts";
-import { ApplicationCommandOptionType, Embed } from "harmony/mod.ts";
+import {
+  ApplicationCommandOptionType,
+  Embed,
+  PermissionFlags,
+} from "harmony/mod.ts";
 
-interface SetLanguageLocale {
-  notAdministrator: string;
-  success: string;
-}
-
-export default new TaprisCommand<SetLanguageLocale>()
+export default new TaprisCommand()
   .setName("setlanguage")
   .setDescription("Set my language for this guild")
   .setOptions({
@@ -19,33 +18,9 @@ export default new TaprisCommand<SetLanguageLocale>()
     type: ApplicationCommandOptionType.STRING,
     required: true,
   })
-  .setLocales({
-    en: {
-      notAdministrator: "You don't have administrator permissions!",
-      success: "Language set to english!",
-    },
-    ru: {
-      notAdministrator: "У вас нет прав администратора!",
-      success: "Язык установлен на русский!",
-    },
-  })
+  .setMemberPermissions(PermissionFlags.ADMINISTRATOR)
   .setGuildOnly()
-  .setRun(async (client, interaction, locale) => {
-    const userMember = await interaction.guild!.members.get(
-      interaction.user.id
-    );
-
-    if (!userMember!.permissions.has("Administrator")) {
-      return await interaction.reply({
-        embeds: [
-          new Embed()
-            .setColor(client.botColor)
-            .setTitle(locale.notAdministrator),
-        ],
-        ephemeral: true,
-      });
-    }
-
+  .setRun(async (client, interaction, _locale) => {
     const language = interaction.options.find(
       (option) => option.name == "language"
     )?.value as LocaleNames;
