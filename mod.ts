c0dcb411b73a681a55cc2602af0a7876da267d93
Alexiy Rybin <0xtapnisu@gmail.com>
2023-05-26 07:58:24 +0700

    Add format

--- mod.ts
+++ mod.ts
@@ -3,4 +3,3 @@ import { TaprisClient } from "./src/mod.ts";
 if (import.meta.main) await new TaprisClient().start();
 
 export * from "./src/mod.ts";
-
--- src/utils/mod.ts
+++ src/utils/mod.ts
@@ -2,4 +2,3 @@ export * from "@utils/api.ts";
 export * from "@utils/config.ts";
 export * from "@utils/db/mod.ts";
 export * from "@utils/generatePassword.ts";
-
