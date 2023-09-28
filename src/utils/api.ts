11d4decdd2071c7e096255e572abe36f261128e8
Aleksei Rybin <0xalekseirybin@gmail.com>
2023-09-28 15:01:11 +0700

    Update domains & mail

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,7 +4,7 @@ import { Hono } from "hono/mod.ts";
 const INVITE_URL =
   "https://discord.com/api/oauth2/authorize?client_id=869088074758520832&scope=bot+applications.commands&permissions=294208515334";
 const GITHUB_URL = "https://github.com/tapris-bot/tapris";
-const WEBSITE_URL = "https://tapris.tapnisu.ru";
+const WEBSITE_URL = "https://tapris.tapni.su";
 
 export class Api extends Hono {
   private client: TaprisClient;
